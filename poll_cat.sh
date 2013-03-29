bb2362792ecca37e5f606d92b383172bd377c7d0
pi <pi@redinger.me>
2013-03-29 14:57:35 -0700

    add upstart script to start poll_cat.sh at boot time. speed up web page generation, upstart must be installed

--- poll_cat.sh
+++ poll_cat.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 while true
 do
-    result=`nfc-poll 2>/dev/null| grep "UID (NFCID1):"` 
+    result=`/usr/local/bin/nfc-poll 2>/dev/null| grep "UID (NFCID1):"` 
     if [[ $? -eq 0 ]] 
     then
         ts=`date +%s`
         id=`echo $result | awk -f /home/pi/workspace/nfc_cats/who_is_it.awk`
         #'BEGIN {FS = " "} {hex=sprintf("0X%s%s%s%s\n", $3,$4,$5,$6);print strtonum(hex)}'`
         echo "$ts $id" >> /home/pi/workspace/nfc_cats/cat.dat
-        echo "$ts $id" 
-        ./catfmt.sh > cat.txt
-        sleep 0.5s
+
+        human=`date -d @"$ts"`
+        result="$human $id"
+        echo $result >> /home/pi/workspace/nfc_cats/cat.txt
     else
       echo "timeout"
     fi
    echo "<pre>" > /var/www/index.lighttpd.html
    tail -n 50 /home/pi/workspace/nfc_cats/cat.txt >> /var/www/index.lighttpd.html 
-   
+   echo "</pre>" >> /var/www/index.lighttpd.html
+   sleep 0.5s
 done
 
