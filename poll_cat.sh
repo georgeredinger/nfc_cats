cdf2baeebc2795d4faae99e4fa426040074a5a1b
George Redinger <george@georgeredinger.com>
2013-04-28 12:58:38 -0700

    a little more testable

--- poll_cat.sh
+++ poll_cat.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-while true
-do
-  /home/$USER/workspace/nfc_cats/read.sh
-  sleep 0.5s
-done
+function watchForCat() {
+  while true
+  do
+      ./read.sh
+      sleep 0.5s
+  done
+}
+
+(
+  if [ "$1" == "test" ]
+  then
+      echo "Testing..."
+      function nfc-poll() {
+        echo "UID (NFCID1): de ad be ef" 
+        return 0
+      }
+      export -f nfc-poll
+      export  PAGE_DESTINATION="./TESTPAGE.HTML"
+      cd /home/george/workspace/nfc_cats
+      (watchForCat & my_pid="$!";sleep 5; kill "$my_pid";echo "enough testing";exit) 
+      firefox "$PAGE_DESTINATION"
+      exit
+  else
+      cd /home/pi/workspace/nfc_cats
+      export  PAGE_DESTINATION="/var/www/index.lighttpd.html"
+      watchForCat
+  fi
+echo "all done"
+)
 
--- read.sh
+++ read.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-result=`/usr/local/bin/nfc-poll 2>/dev/null| grep "UID (NFCID1):"` 
+result=`nfc-poll 2>/dev/null| grep "UID (NFCID1):"` 
 if [[ $? -eq 0 ]] 
 then
     ts=`date +%s`
-    id=`echo $result | awk -f /home/$USER/workspace/nfc_cats/who_is_it.awk`
-    echo "$ts $id" >> /home/$USER/workspace/nfc_cats/cat.dat
-    /home/$USER/workspace/nfc_cats/process.sh $ts $id
+    id=`echo $result | awk -f ./who_is_it.awk`
+    echo "$ts $id" >> ./cat.dat
+    ./process.sh $ts $id
 else
     echo "timeout"
 fi
